a28a6416cfad6511abe2f75aaa8409f1cf3ca6a5
Elana Hashman <elana@hashman.ca>
2015-10-27 13:23:04 -0600

    Add test for empty tokens and fix bug
    Currently, the empty string '' is a permissible token. That should not be the
    case. By writing this test case, I found this bug and then fixed it.

--- test_worker.py
+++ test_worker.py
@@ -10,6 +10,8 @@ class TestWorker(unittest.TestCase):
     def test_single_word(self):
         self.assertEqual(word_count('WoRd'), {'word': 1})
 
+    def test_no_empty_strings(self):
+        self.assertEqual(word_count(''), {})
 
 
 # Run unit tests.
--- worker.py
+++ worker.py
@@ -5,7 +5,10 @@ import re
 
 def word_count(text_blob):
     word_map = {}
-    tokens = [x.lower() for x in re.split('[^a-zA-Z0-9]+', text_blob)]
+
+    # filter(bool, list) removes empty strings, which evaluate to False.
+    tokens = [x.lower() for x in
+              filter(bool, re.split('[^a-zA-Z0-9]+', text_blob))]
 
     for token in tokens:
         if word_map.get(token) is None:
