8cc0a85e17adbb6b0beae6ea2d948b7efcb21974
Elana Hashman <elana@hashman.ca>
2015-10-27 13:53:07 -0600

    Implement reduce_word_count and simple tests
    The design document explicitly references "map" as the data type for the
    various hashes containing word counts. I found an excellent built-in Python
    data type, Counter, that is specialized for our use case. For now, I am using
    it internally in collector.py, but I think it may make sense to change the data
    type project-wide in a later commit.

---
+++ test_collector.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2015 Elana Hashman
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from collector import reduce_word_count
+
+
+class TestWorker(unittest.TestCase):
+    def test_single_dict(self):
+        self.assertEqual(reduce_word_count([{'word': 1}]), {'word': 1})
+
+    def test_two_dicts(self):
+        self.assertEqual(
+            reduce_word_count([{'word': 1}, {'word': 2}]), {'word': 3})
+
+
+# Run unit tests.
+if __name__ == '__main__':
+    unittest.main()
