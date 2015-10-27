42770686a5db68d6a7bc503cd407adafe9610e3b
Elana Hashman <elana@hashman.ca>
2015-10-27 13:26:00 -0600

    Add a few more tests

--- test_worker.py
+++ test_worker.py
@@ -1,6 +1,7 @@
 #
 #
 
+import json
 import unittest
 
 from worker import word_count
@@ -12,6 +13,26 @@ class TestWorker(unittest.TestCase):
 
     def test_no_empty_strings(self):
         self.assertEqual(word_count(''), {})
+
+    def test_punctuation_only(self):
+        self.assertEqual(word_count(' .;$!!!!   	"""'), {})
+
+    def test_on_a_link(self):
+        self.assertEqual(
+            word_count('http://hashman.ca'),
+            {
+                'http': 1,
+                'hashman': 1,
+                'ca': 1
+            })
+
+    def test_AModestProposal(self):
+        book = open('test_data/a_modest_proposal.txt', 'r')
+        text = book.read()
+
+        word_map = json.load(open('test_data/a_modest_proposal.json', 'r'))
+
+        self.assertEqual(word_count(text), word_map)
 
 
 # Run unit tests.
